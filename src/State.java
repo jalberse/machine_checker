0c274f93baaf3c3e65ea27c18c931cfb0f85865e
John Alberse <jalberse@gmail.com>
2018-12-07 14:17:16 -0600

    Change transition set for state from array to hashmap

--- src/State.java
+++ src/State.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 
 /*
     Models a single state of a machine (vertex in its graph)
@@ -6,8 +7,21 @@ import java.util.ArrayList;
     @author John Alberse
 */
 public class State {
-    private ArrayList<Edge> transitions;
-    private String label;
+    private int id; // Unique identifier for the state
+    private HashMap transitions = new HashMap(); // <char, Transtion>
+    private boolean accepting; // True if element of set of accepting states
 
+    public State(int id) {
+        this.id = id;
+        this.accepting = false;
+    }
 
+    /*
+    Adds a transition function to the state. If the input symbol for the provided transition already has a transition
+        on this state, the old transition is replaced.
+    @param transition The transition to be added
+     */
+    public void addTransition(Transition transition) {
+        transitions.put(transition.getInputSymbol(),transition);
+    }
 }
