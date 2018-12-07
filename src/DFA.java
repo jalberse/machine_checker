c71fb41e76a234aa18d5582d5cf720a3255c1272
John Alberse <jalberse@gmail.com>
2018-12-07 15:36:49 -0600

    Create DFA class file

---
+++ src/DFA.java
@@ -0,0 +1,16 @@
+/*
+    A deterministic finite automata
+    M = (Q, A, T, q0, F) where:
+        Q is the set of states
+        A is the input alphabet, and is implicitly defined as all input symbols part of transitions added to the machine
+        T is the transition function T: (Q x A) -> Q. Transitions are associated with each state and sent to other
+            states on reading an input symbol
+        q0 is the initial state
+        F is the set of accepting states. Implicitly defined by having each state track whether or not they accept
+
+    @author John Alberse
+    @date 12/7/2018
+ */
+public class DFA {
+    
+}
