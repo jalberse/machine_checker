66870f1fd1c2c227ed1acf8bb5095268a827ff39
John Alberse <jalberse@gmail.com>
2018-12-07 16:26:13 -0600

    Restructure. DFA has HashSet rather than HashMap of states, so no longer dynamically add

--- src/DFA.java
+++ src/DFA.java
@@ -1,4 +1,5 @@
 import java.util.HashMap;
+import java.util.HashSet;
 
 /*
     A deterministic finite automata
@@ -16,7 +17,7 @@ import java.util.HashMap;
 public class DFA {
     private State startState;
     private State currentState;
-    private HashMap<Integer,State> states;
+    private HashSet<State> states;
     int size; // the number of states
 
     /*
@@ -25,38 +26,9 @@ public class DFA {
     public DFA(){
         this.startState = null;
         this.currentState = null;
-        states = new HashMap<Integer,State>();
+        states = new HashSet<State>();
     }
 
-    /*
-        Adds the state to the machine. If this is the first state added to the machine, it is set as the starting state
-        and the current state.
-        If there is already a state in the machine with the same ID, it is replaced.
-     */
-    public void addState(State state){
-        if (startState == null) {
-            startState = state;
-            currentState = state;
-        }
-        if (states.put(state.getId(),state) != null) ++size; // add the state and increment size if succesful
-    }
-
-
-    /*
-        Adds a transition from the given state to the target state specified by the transition.
-        If the target state is not already a state in the machine, it is added.
-
-        @param state the state to add a transition from
-        @param transition the transition from the state to another
-     */
-    public void addTransition(State fromState, Transition transition) {
-        if (!states.containsKey(transition.getResult().getId())) {
-            // If the state is not already in the machine, add it
-            this.addState(transition.getResult());
-        }
-        // Associate the transition with fromState
-        states.get(fromState.getId()).addTransition(transition);
-    }
 
     // Getters
     public State getStartState() {
