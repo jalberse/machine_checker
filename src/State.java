4bcc2a98ff6b42b505981fc8130f55225aaa00fd
John Alberse <jalberse@gmail.com>
2018-12-07 13:47:28 -0600

    Create state class

---
+++ src/State.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+/*
+    Models a single state of a machine (vertex in its graph)
+
+    @author John Alberse
+*/
+public class State {
+    private ArrayList<Edge> transitions;
+    private String label;
+
+
+}
