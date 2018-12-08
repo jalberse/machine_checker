7ffc8f11dbb9eb887d871f97bfff0d2152ad814b
John Alberse <alberse.john@gmail.com>
2018-12-08 16:00:33 -0600

    Begin major refactor, move state and transition rep directly into DFA class. Unnecessary level of abstraction to give each of those their own classes

--- src/DFA.java
+++ src/DFA.java
@@ -1,5 +1,4 @@
 import java.util.HashSet;
-import java.util.Iterator;
 
 /*
     A deterministic finite automata
@@ -15,30 +14,18 @@ import java.util.Iterator;
     @date 12/7/2018
  */
 public class DFA {
-    private State startState;
-    private State currentState;
-    private HashSet<State> states;
+    final private int startState = 0;
+    private int size;
+    private int currentState;
+    private HashSet<Integer> acceptingStates;
 
     /*
         Creates a DFA with no states or transitions
      */
     public DFA(){
-        this.startState = null;
-        this.currentState = null;
-        this.states = new HashSet<State>();
+        // TODO: This
     }
 
-    /*
-        Create a DFA with the given start state and map of states
-
-        @param startState The start state
-        @states the states of the machine
-     */
-    public DFA(State startState, HashSet<State> states) {
-        this.startState = startState;
-        this.currentState = startState;
-        this.states = states;
-    }
 
     /*
         Execute one step of the DFA, i.e. read the inputSymbol and change state accordingly
@@ -46,46 +33,18 @@ public class DFA {
         @param inputSymbol the input symbol. New state is determined by T(currentState, inputSymbol)
      */
     public void readSymbol(Character inputSymbol) throws NoSuchTransitionException{
-        currentState = currentState.getNextState(inputSymbol);
-        if (currentState == null) {
-            // There is no transition for this symbol on this state
-            // i.e. the word is NOT an element of L(M)
-            throw new NoSuchTransitionException(String.format("No such transition for %c on q%d%n",inputSymbol,currentState.getId()));
-        }
-    }
 
-    /*
-        Adds the state to the machine. If this is the first state added to the machine, it is set as the starting state
-        and the current state.
-        If there is already a state in the machine with the same ID, it is replaced.
-     */
-    public void addState(State state){
-        if (startState == null) {
-            startState = state;
-            currentState = state;
-        }
-        states.add(state);
     }
-
-    // TODO: Add and remove states
 
     // Getters
-    public State getStartState() {
+    public Integer getStartState() {
         return this.startState;
     }
-    public State getCurrentState() {
+    public Integer getCurrentState() {
         return this.currentState;
     }
     public int getSize(){
-        return states.size();
-    }
-
-    // Setters
-    public void setStartState(State state) {
-        this.startState = state;
-    }
-    public void setCurrentState(State state) {
-        this.currentState = state;
+        return size;
     }
 
     /*
@@ -93,14 +52,7 @@ public class DFA {
      */
     public String toString(){
         StringBuffer buf = new StringBuffer();
-        buf.append(String.format("Start state: %d%n",startState.getId()));
-        buf.append(String.format("Current state: %d%n",currentState.getId()));
-        buf.append(String.format("Size: %d%nSTATES%n",states.size()));
-        Iterator<State> it = states.iterator();
-        while(it.hasNext()){
-            State temp = it.next();
-            buf.append(temp.toString());
-        }
+        // TODO: This
         return buf.toString();
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,5 @@
 public class Main {
     public static void main(String[] args) {
         DFA dfa = new DFA();
-        State state0 = new State();
-        State state1 = new State(1);
-        dfa.addState(state0);
-        dfa.addState(state1);
     }
 }
