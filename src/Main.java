8ecd921d59b9ff1db92e1adbad393c974d1cacbf
John Alberse <alberse.john@gmail.com>
2018-12-08 20:34:53 -0600

    DFA toString error fix -  to %s

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         DFA dfa = new DFA();
+        dfa.setDescription("This is a test dfa");
         dfa.addState(0,false);
         dfa.addState(1,true);
         System.out.println(dfa);
