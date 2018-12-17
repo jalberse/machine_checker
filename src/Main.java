9157ab0852651149ca58c67a61b9b3b3bab00ac0
John Alberse <alberse.john@gmail.com>
2018-12-17 14:12:08 -0600

    Set up testing

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        DFA dfa = new DFA ("dfa_example.txt");
-        System.out.println(dfa);
-
+        
     }
 }
---
+++ test/DFATest.java
@@ -0,0 +1,36 @@
+import static org.testng.Assert.*;
+
+public class DFATest {
+
+    @org.testng.annotations.Test
+    public void testRun() {
+    }
+
+    @org.testng.annotations.Test
+    public void testSetInputAlphabet() {
+    }
+
+    @org.testng.annotations.Test
+    public void testAddState() {
+    }
+
+    @org.testng.annotations.Test
+    public void testAddTransition() {
+    }
+
+    @org.testng.annotations.Test
+    public void testSetDescription() {
+    }
+
+    @org.testng.annotations.Test
+    public void testGetCurrentState() {
+    }
+
+    @org.testng.annotations.Test
+    public void testGetSize() {
+    }
+
+    @org.testng.annotations.Test
+    public void testToString() {
+    }
+}
