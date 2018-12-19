29b937f619423d59e71af8c9fc6ebd262a164c25
John Alberse <alberse.john@gmail.com>
2018-12-19 09:59:17 -0600

    Begin GUI

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import javax.swing.*;
+import javax.swing.SwingUtilities;
+
 public class Main {
     public static void main(String[] args) {
+
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                JFrame frame = new JFrame("Automata");
+                frame.setSize(600,500);
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.setVisible(true);
+            }
+        });
 
     }
 }
