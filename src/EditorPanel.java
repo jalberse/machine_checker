21964be2952a330f1bef4b367e305a6fa9cca94c
John Alberse <alberse.john@gmail.com>
2018-12-19 17:50:03 -0600

    Inline TODOs

--- src/EditorPanel.java
+++ src/EditorPanel.java
@@ -5,11 +5,13 @@ import java.util.ArrayList;
 public class EditorPanel extends JPanel {
 
     // TODO: Will need tooltip and block update button when anything not valid
+    // TODO: InputVerifier subclass. Check for where to refactor to a formatted text field
 
     private LabelledTextArea descriptionArea;
     private LabelledSpinner numberOfStatesSpinner;
     private LabelledTextField inputAlphabetField;
     // TODO: Array of transition function panels which have 3 components for inputs and outputs?
+    //       TODO: There should be a sub-panel for each state's transitions for ease of use
     private LabelledChecklist acceptingStates;
     private JButton updateMachineButton;
 
@@ -19,7 +21,7 @@ public class EditorPanel extends JPanel {
         descriptionArea = new LabelledTextArea("Description");
         numberOfStatesSpinner = new LabelledSpinner("Number of States");
         inputAlphabetField = new LabelledTextField("Input Alphabet");
-        acceptingStates = new LabelledChecklist("Accepting States");
+        acceptingStates = new LabelledChecklist("Accepting States"); // TODO: Wrap in scroll, could be large
         updateMachineButton = new JButton("Update Machine");
 
         add(descriptionArea);
---
+++ src/TransitionPanel.java
@@ -0,0 +1,5 @@
+import javax.swing.*;
+
+public class TransitionPanel extends JPanel {
+
+}
